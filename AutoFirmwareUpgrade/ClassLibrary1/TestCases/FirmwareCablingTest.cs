f2ecbce1079352872b17b8d100baffa5c7ae53b3
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-07-13 16:43:50 +0530

    Added method for Loading default calibration
    a) Added method for Loading default calibration on Mfgindex
    b) Added variable to fetch data from Testdata-DeviceIP and Calibration
    file path.

--- AutoFirmwareUpgrade/ClassLibrary1/TestCases/FirmwareCablingTest.cs
+++ AutoFirmwareUpgrade/ClassLibrary1/TestCases/FirmwareCablingTest.cs
@@ -7,7 +7,9 @@ using OpenQA.Selenium.Chrome;
 using Tabindex_Configuration.dfr;
 using Tabindex_Data.pmp;
 using Tabindex_Data.soh;
+using Mfgindex.Calibration;
 using CashelFirmware.Utility;
+using System.IO;
 
 namespace CashelFirmware.NunitTests
 {
@@ -18,7 +20,10 @@ namespace CashelFirmware.NunitTests
        Ping pinger = new Ping();
        PingReply GetDevicePingReplySuccess;
        Read_WriteExcel rdexcel;
+       string DataSetFileNameWithPath;
        System.Resources.ResourceManager resourceManager;
+       public string deviceIP=string.Empty;
+
 
         public FirmwareCablingTest()
         {
@@ -42,6 +47,7 @@ namespace CashelFirmware.NunitTests
 
             webdriver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, options);
             webdriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(25)); //Implicit wait is added so that selenium doesn't fail if any element is not loaded within specified time interval.
+            //deviceIP = "10.75.58.51";
         }
         /// <summary>
         /// This method is called at the end of all the test so that google Chrome can be closed.
@@ -61,11 +67,14 @@ namespace CashelFirmware.NunitTests
         [Test]       
         public void Cabling3U()
         {
-            string deviceIP = "10.75.58.51";
-            string filename = @"C:\Users\rdev\Documents\My Received Files\3U_Standalone.xlsx";
+            DataSetFileNameWithPath = AppDomain.CurrentDomain.BaseDirectory + @"\TestDataFiles\CablingDataSet\3U_Standalone.xlsx";
+            string deviceIP = rdexcel.ReadExcel(DataSetFileNameWithPath, "DeviceInfo",0,"DeviceIP");
+
             Tabindex_Configuration_dfr Tabindex_Configuration_dfr = new Tabindex_Configuration_dfr(webdriver);
             Tabindex_Data_pmp Tabindex_Data_pmp = new Tabindex_Data_pmp(webdriver);
             Tabindex_Data_soh Tabindex_Data_soh = new Tabindex_Data_soh(webdriver);
+
+            Assert.IsTrue(UploadCalbirationFile_3U_15I(deviceIP));
 
             Assert.AreEqual("Configuration", Tabindex_Configuration_dfr.OpenTabIndexPage(deviceIP),"Device is up/responding");
             Assert.IsTrue(Tabindex_Configuration_dfr.Item_Configuration_Click(), "Clicked on Configuration button on webpage");
@@ -77,10 +86,10 @@ namespace CashelFirmware.NunitTests
             for (int i = 0; i < 18; i++)
             {
                 Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_Click(i+1), "Clicked on Channel[" + i +"] option");
-                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_busbar_Select((i), filename),"Set the busbar number for iteration "+i);
-                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_feeder_Click((i), filename),"Set the feeder number for iteration " + i);
-                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_phase_Click((i), filename), "Set the phase name for iteration " + i);
-                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_usage_Click((i), filename), "Set the usage name for iteration " + i);
+                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_busbar_Select((i), DataSetFileNameWithPath),"Set the busbar number for iteration "+i);
+                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_feeder_Click((i), DataSetFileNameWithPath),"Set the feeder number for iteration " + i);
+                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_phase_Click((i), DataSetFileNameWithPath), "Set the phase name for iteration " + i);
+                Assert.IsTrue(Tabindex_Configuration_dfr.Item_dfr_analog_channel_usage_Click((i), DataSetFileNameWithPath), "Set the usage name for iteration " + i);
             }
             Assert.IsTrue(Tabindex_Configuration_dfr.SwitchToParentFrame(),"Successfully switched to parent frame");
             Assert.IsTrue(Tabindex_Configuration_dfr.Commit_Click(), "Clicked on Commit");
@@ -111,8 +120,23 @@ namespace CashelFirmware.NunitTests
             Assert.IsTrue(Tabindex_Data_pmp.Item_pmp_Click(), "Clicked on pmp in topology");
             Assert.IsTrue(Tabindex_Data_pmp.SwitchFrame_Frompmp_Todata(), "Switch frame from pmp to pmp data");
             Assert.IsTrue(Tabindex_Data_pmp.Item_pmp_data_Click(), "Clicked on pmp data");
-            Assert.AreEqual(rdexcel.ReadExcel(filename, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), 0, resourceManager.GetString("EXCELDATA_PQCABLING").ToString()), Tabindex_Data_pmp.Get_PQCabling());
-            Assert.AreEqual(rdexcel.ReadExcel(filename, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), 0, resourceManager.GetString("EXCELDATA_FRCABLING").ToString()), Tabindex_Data_pmp.Get_FRCabling());
+            Assert.AreEqual(rdexcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), 0, resourceManager.GetString("EXCELDATA_PQCABLING").ToString()), Tabindex_Data_pmp.Get_PQCabling());
+            Assert.AreEqual(rdexcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), 0, resourceManager.GetString("EXCELDATA_FRCABLING").ToString()), Tabindex_Data_pmp.Get_FRCabling());
+        }
+
+        
+        private bool UploadCalbirationFile_3U_15I(string deviceIP)
+        {
+            string calibrationfilenamewithpath = AppDomain.CurrentDomain.BaseDirectory + @"\TestDataFiles\CablingDataSet\" + rdexcel.ReadExcel(DataSetFileNameWithPath, "DeviceInfo", 0, "CalibrationFileName");
+            Mfgindex_Calibration Mfgindex_Calibration = new Mfgindex_Calibration(webdriver);
+            Assert.AreEqual("Calibration", Mfgindex_Calibration.OpenMfgindexPage(deviceIP));
+            Assert.IsTrue(Mfgindex_Calibration.Btn_Calibration_Click());
+            Assert.IsTrue(Mfgindex_Calibration.SwitchFrame_FromCalibration_ToENGLISH());
+            Assert.IsTrue(Mfgindex_Calibration.Btn_ENGLISH_Click());
+            Assert.IsTrue(Mfgindex_Calibration.Btn_LoadCalibration_Click());
+            Assert.IsTrue(Mfgindex_Calibration.UploadFile(calibrationfilenamewithpath));
+            Assert.IsTrue(Mfgindex_Calibration.Btn_Load_Click());
+            return true;
         }
     }
 }
