7b4c8a495fcfc5552eb90619619b1cbdd726430d
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-08-13 17:07:18 +0530

    Added method for later use

--- AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/Utility/ScreenShotCapture.cs
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/Utility/ScreenShotCapture.cs
@@ -6,18 +6,17 @@ using System.Drawing.Imaging;
 
 namespace CashelFirmware.Utility
 {
-    class ScreenShotCapture
-    {
-        
-        public static string Capture(IWebDriver webdriver, string screenShotName)
+    public static class ScreenShotCapture
+    {        
+        public static void Capture(IWebDriver webdriver, string screenShotName)
         {
+            webdriver.Manage().Window.Size = new System.Drawing.Size(1024, 768);
             ITakesScreenshot takesScreenshot = (ITakesScreenshot)webdriver;
             Screenshot screenshot = takesScreenshot.GetScreenshot();
             string pth = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
-            string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "ErrorScreenshots\\" + screenShotName + ".png";
+            string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "ScreenShot\\" + screenShotName + ".png";
             string localpath = new Uri(finalpth).LocalPath;
             screenshot.SaveAsFile(localpath, ImageFormat.Png);
-            return localpath;
         }
     }
 }
