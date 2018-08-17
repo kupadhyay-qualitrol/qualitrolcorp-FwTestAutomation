8b89437712ae56d075a0b2c59143e4d3f999b7a3
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-08-17 07:23:33 +0530

    Renamed the TestSuite with CablingSuite to have this class only Test cases related to cabling

--- AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/TestSuite/CablingSuite.cs
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/TestSuite/CablingSuite.cs
@@ -1,76 +1,19 @@
-﻿/*This file contains all the test cases as per Nunit Framework
+﻿/*This file contains all the test cases as per Nunit Framework for Cabling
  */
 
-using System;
-using CashelFirmware.NunitTests;
 using NUnit.Framework;
 using CashelFirmware.Reporting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using RelevantCodes.ExtentReports;
-using Microsoft.Office.Interop.Excel;
-using CashelFirmware.Utility;
+using CashelFirmware.NunitTests;
 
 namespace CashelFirmware.TestSuite
 {
-    public class TestSuite
+    public class CablingSuite:BaseTestSuite
     {
-        public ExtentTest InfovarStartTest;
-        FirmwareCablingTest CablingTest;
-        IWebDriver webdriver;
-        public string deviceIP = string.Empty;
+        public FirmwareCablingTest CablingTest;
 
-
-        public TestSuite()
+        public CablingSuite()
         {
             CablingTest = new FirmwareCablingTest();
-            Read_WriteExcel.xlapp = new Application();
-        }
-
-        [SetUp]
-        public void TestSetup()
-        {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArguments(new[] {
-                "start-maximized",
-                "allow-running-insecure-content",
-                "test-type","ignore-certificate-errors","disable-extensions"});
-            options.AddUserProfilePreference("credentials_enable_service", false);
-            options.AddUserProfilePreference("profile.password_manager_enabled", false);
-
-            webdriver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, options);
-            webdriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(25)); //Implicit wait is added so that selenium doesn't fail if any element is not loaded within specified time interval.
-            deviceIP = "10.75.58.51";
-        }
-
-        [OneTimeSetUp]
-        public void StartReport()
-        {
-            ReportGeneration.StartReport("CablingTest");
-        }
-
-        [OneTimeTearDown]
-        public void EndReprot()
-        {
-            ReportGeneration.EndReprot();
-
-            System.Diagnostics.Process[] AllProcesses = System.Diagnostics.Process.GetProcessesByName("excel");
-
-            // check to kill the right process
-            foreach (System.Diagnostics.Process ExcelProcess in AllProcesses)
-            {
-                if (ExcelProcess.ProcessName.Equals("EXCEL"))
-                    ExcelProcess.Kill();
-            }
-            AllProcesses = null;
-
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            ReportGeneration.EndTestCaseReport(InfovarStartTest,webdriver);
-            webdriver.Quit();
         }
 
         [Test, Order(1)]
