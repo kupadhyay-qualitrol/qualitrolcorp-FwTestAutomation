3fbd90b491d7033af6782fc5283a8a806a96aa01
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-08-23 14:59:03 +0530

    Code to getch calculation table for pqp for each cabling

--- AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/TestCode/GetPQP_CalculationTable.cs
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/TestCode/GetPQP_CalculationTable.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* This file is used to fetch dynamic pqp table from tabindex and store in a .txt file
+ */
+
+using System;
 using Tabindex_Data.pqp;
 using OpenQA.Selenium;
 using NUnit.Framework;
 using RelevantCodes.ExtentReports;
 using System.IO;
+using System.Text;
+using System.Xml;
 
 namespace CashelFirmware.NunitTests
 {
     public class GetPQP_CalculationTable
     {
+
         public void GetPQPDynamicTable(IWebDriver webDriver,string deviceIP, ExtentTest TestLog,string Cabling)
         {
+            string paramtype = string.Empty;
+            string phase = string.Empty;
+            string harmonicrank = string.Empty;
+            string busbarfeeder_type = string.Empty;
+
+            StringBuilder pqpdata = new StringBuilder();
+
+            string DataSetFileName= AppDomain.CurrentDomain.BaseDirectory + @"\TestDataFiles\CablingDataSet\" + Cabling + ".xlsx";
             Tabindex_Data_pqp Tabindex_Data_Pqp = new Tabindex_Data_pqp(webDriver);
             Assert.AreEqual("Data", Tabindex_Data_Pqp.OpenTabIndexPage(deviceIP), "Device is up/responding");
             TestLog.Log(LogStatus.Pass, "Device is up/responding");
@@ -37,58 +47,30 @@ namespace CashelFirmware.NunitTests
             Assert.IsTrue(Tabindex_Data_Pqp.Item_Calculation_Type_Click(), "Clicked on Calculation type");
             TestLog.Log(LogStatus.Pass, "Success:-Clicked on Calculation type");
             string Filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Cabling + ".txt");
-            //File.Create(Filepath);
-                        
-            for (int calcindex = 0; calcindex < 3700; calcindex++)
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("http://" + deviceIP + "/cgi-bin/ipcxml.cgi?pqp:pqp/data");
+
+            XmlNodeList nodelist = xmlDocument.GetElementsByTagName("calc_type");
+
+            foreach (XmlNode xml in nodelist)
             {
-                string paramtype =string.Empty;
-                string phase = string.Empty;
-                string harmonicrank = string.Empty;
-                string busbarfeeder_type = string.Empty;
-
-                //Assert.IsTrue(Tabindex_Data_Pqp.Item_Calc_type_num_Click(calcindex), "Clicked on Calc num[" + calcindex + "]");
-
-                paramtype = Tabindex_Data_Pqp.Get_paramtype(calcindex);
-                harmonicrank = Tabindex_Data_Pqp.Get_harmonicrank(calcindex);
-                busbarfeeder_type = Tabindex_Data_Pqp.Get_busbarfeedertype(calcindex);
-                switch (Tabindex_Data_Pqp.Get_phase(calcindex))
-                {
-                    case "NONE":
-                        {
-                            phase = "0";
-                            break;
-                        }
-                    case "A":
-                        {
-                            phase = "1";
-                            break;
-                        }
-                    case "B":
-                        {
-                            phase = "2";
-                            break;
-                        }
-                    case "C":
-                        {
-                            phase = "3";
-                            break;
-                        }
-                    case "N":
-                        {
-                            phase = "4";
-                            break;
-                        }
-                    default:
-                        break;
-                }
-
-                using (StreamWriter Filewrite = File.AppendText(Filepath))
-                {
-                    Filewrite.WriteLine("{" + paramtype + "," + phase + "," + harmonicrank + "," + busbarfeeder_type + "},");
-                }
+                paramtype = string.Empty;
+                phase = string.Empty;
+                harmonicrank = string.Empty;
+                busbarfeeder_type = string.Empty;
+                paramtype = xml.ChildNodes[0].InnerText.Trim();
+                phase= xml.ChildNodes[1].InnerText.Trim();
+                harmonicrank= xml.ChildNodes[2].InnerText.Trim();
+                busbarfeeder_type= xml.ChildNodes[3].InnerText.Trim();
+                pqpdata.AppendLine("{" + paramtype + "," + phase + "," + harmonicrank + "," + busbarfeeder_type + "},");
             }
-            TestLog.Log(LogStatus.Pass, "Created File Successfully for :-" + Cabling);
+ 
+            StreamWriter Filewrite = new StreamWriter(Filepath);            
+            Filewrite.WriteLine(pqpdata.ToString());
+            Filewrite.Close();
+            
+            TestLog.Log(LogStatus.Pass, "Created File Successfully for :-" + Cabling); 
         }
-
     }
 }
