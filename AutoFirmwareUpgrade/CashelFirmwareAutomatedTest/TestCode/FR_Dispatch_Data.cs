cd3e8999b4ec0f94f6019831b44b72e33f8bda57
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-09-12 15:25:22 +0530

    EP-1576 Initialising under the constructor

--- AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/TestCode/FR_Dispatch_Data.cs
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/TestCode/FR_Dispatch_Data.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Tabindex_Data.dispatch;
 using Tabindex_Data.pmp;
 using OpenQA.Selenium;
@@ -20,20 +19,26 @@ namespace CashelFirmware.NunitTests
     {
         ResourceManager resourceManager;
         string DataSetFileNameWithPath;
-        string[] FR_Dispactch_RMS_Data = new string[18];
+        string[] FR_Dispactch_RMS_Data;
         Dictionary<string, string> CablingInfo;
         StringBuilder FR_Data_RMS_Value;
-        int[] TXRatioMultiplier = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
-        double injectvoltage = 50.0;
-        double injectedcurrent = 1.0;
-        string[] channeltype = new string[18];
-        Dictionary<string,int> Channel_TXRatio=new Dictionary<string,int>();
+        int[] TXRatioMultiplier;
+        double injectvoltage;
+        double injectedcurrent;
+        string[] channeltype;
+        Dictionary<string, int> Channel_TXRatio;
 
         public FR_Dispatch_Data()
         {
-            resourceManager = new System.Resources.ResourceManager("CashelFirmwareAutomatedTest.Resource", this.GetType().Assembly);
+            resourceManager = new ResourceManager("CashelFirmwareAutomatedTest.Resource", this.GetType().Assembly);
             CablingInfo = new Dictionary<string, string>();
-            FR_Data_RMS_Value = new StringBuilder();                
+            Channel_TXRatio = new Dictionary<string, int>();
+            FR_Data_RMS_Value = new StringBuilder();
+            FR_Dispactch_RMS_Data = new string[18];
+            injectvoltage = 50.0;
+            injectedcurrent = 1.0;
+            channeltype = new string[18];
+            TXRatioMultiplier = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
         }
 
         public void Validate_FR_Dispatch_Data(IWebDriver webDriver, string deviceIP, ExtentTest TestLog, string Cabling)
@@ -76,12 +81,19 @@ namespace CashelFirmware.NunitTests
             {
                 FR_Dispactch_RMS_Data[index] = Tabindex_Data_Dispatch.Get_Magnitude(index);
             }
-
+            CablingInfo.Clear();
+            Channel_TXRatio.Clear();
             for (int channelindex = 0; channelindex < 18; channelindex++)
             {
                 channeltype[channelindex] = Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_si_units").ToString());
-                Channel_TXRatio.Add(Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_label").ToString()), TXRatioMultiplier[channelindex]);
-
+                if (!Channel_TXRatio.ContainsKey(Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_label").ToString())))
+                {
+                    Channel_TXRatio.Add(Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_label").ToString()), TXRatioMultiplier[channelindex]);
+                }
+                else
+                {
+                    TestLog.Log(LogStatus.Info,"Key is :- "+ Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_label").ToString()));
+                }
                 if ((Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_busbar").ToString()) == "BUSBAR1") &&
                     (Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_feeder_number").ToString()) == "NO_FEEDER") &&
                     ((Read_WriteExcel.ReadExcel(DataSetFileNameWithPath, resourceManager.GetString("EXCELDATA_SHEETNAME_CABLING").ToString(), channelindex, resourceManager.GetString("EXCELDATA_CABLING_usage").ToString()) == "BOTH")))
@@ -192,8 +204,8 @@ namespace CashelFirmware.NunitTests
                     }
                 }      
             }
-                );
-
+                );             
+            sortedDict = null;
         }
 
         private int ChannelMultiplier(string channellabel)
