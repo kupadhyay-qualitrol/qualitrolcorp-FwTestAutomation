80753eea60551dbb5272c44c6a90ba0d5908648b
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-11-30 08:00:43 +0530

    Two new Scripts added for Config Editor Objects and Fault Recording page

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/ConfigEditorPage.js
@@ -0,0 +1,58 @@
+﻿/*
+This page contains objects & method related to Config Editor.
+Like the buttons/edit box Send to Device,Save to database, Close which are general.
+*/
+//USEUNIT CommonMethod
+
+var Btn_SendToDevice =Aliases.iQ_Plus.Form.Confgiuration.ConfigEditor.pnlPaddingControl.gbxBorder.frmBottom.btnClose
+//This method is used to click on Send to Device button
+function ClickSendToDevice()
+{
+  if (Btn_SendToDevice.Exists)
+  {
+    Btn_SendToDevice.Click()
+    Log.Message("Clicked on Send to Device button")
+    CommonMethod.CheckActivityLog("Configuration assigned successfully to device")
+    return true
+  }
+  else
+  {
+    Log.Message("Send to Device button doesn't exist/displayed")
+    return false
+  }
+}
+
+var Btn_SaveToDb =Aliases.iQ_Plus.Form.Confgiuration.ConfigEditor.pnlPaddingControl.gbxBorder.frmBottom.btnSaveToDb
+//This method is used to click on Save to Database button
+function ClickSaveToDb()
+{
+  if (Btn_SaveToDb.Exists)
+  {
+    Btn_SaveToDb.Click()
+    CommonMethod.CheckActivityLog("Configuration saved successfully for device")
+    Log.Message("Clicked on Save to Database button.")
+    return true
+  }
+  else
+  {
+    Log.Message("Send to Device button doesn't exist/displayed.")
+    return false
+  }
+}
+
+var Btn_Close =Aliases.iQ_Plus.Form.Confgiuration.ConfigEditor.pnlPaddingControl.gbxBorder.frmBottom.btnClose
+//This method is used to click on Close button
+function ClickOnClose()
+{
+  if (Btn_Close.Exists)
+  {
+    Btn_Close.Click()
+    Log.Message("Clicked on Close button")
+    return true
+  }
+  else
+  {
+    Log.Message("Close button doesn't exist/displayed")
+    return false
+  }
+}
---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/ConfigEditor_FaultRecordingPage.js
@@ -0,0 +1,48 @@
+﻿/*This page contains objects and methods related to Configuration Editor
+*/
+
+//This method is used to Click on Fault Recording
+var Item_ConfigurationEditor = Aliases.iQ_Plus.Form
+var Item_ConfigTree=Aliases.iQ_Plus.Form.Confgiuration.ConfigEditor.pnlPaddingControl.gbxBorder.scContainer.SplitterPanel.configTree
+function ClickOnFaultRecording()
+{
+  if(Item_ConfigTree.Exists)
+  {
+    Item_ConfigTree.ClickItem("Fault Recording")
+    Log.Message("Clicked on Fault Recording")
+    return true
+  }
+  else
+  {
+    Log.Message("Unable to click on Fault Recording")
+    return false
+  }
+}
+
+var Edtbx_Prefault =Aliases.iQ_Plus.Form.Confgiuration.ConfigEditor.pnlPaddingControl.gbxBorder.scContainer.SplitterPanel2.grpConfigPane.frmContainer.ucFrGeneral.frmBackground.frmPrefaultTimeAndSampleRate.txtPrefault
+//This method is used to Set Prefault time for DFR.
+function SetPrefault(PrefaultTime)
+{
+  //Click on Fault Recording in Config Editor
+  if(ClickOnFaultRecording())
+  {  
+    if(PrefaultTime!=null)
+    {
+      Edtbx_Prefault.Text=PrefaultTime
+      Log.Message("Able to set the Prefault time to:- "+PrefaultTime)
+      Log.Picture(Item_ConfigurationEditor,"Snapshot for Prefault time")
+      return true
+    }
+    else
+    {
+     Log.Message("Prefault time is null.")
+     return false
+    }
+  }
+  else
+  {
+    Log.Picture(Item_ConfigurationEditor,"Sanpshot for debug")
+    return false
+  }
+  
+}
