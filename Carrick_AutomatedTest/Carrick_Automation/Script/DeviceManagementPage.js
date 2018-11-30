72ad65edcb2bb9e3ca0bc5cee14ffe7c30e9842a
rdev-qualitrol <rdev@qualitrolcorp.com>
2018-11-30 07:58:09 +0530

    Appended the message for retrieving config

--- Carrick_AutomatedTest/Carrick_Automation/Script/DeviceManagementPage.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/DeviceManagementPage.js
@@ -4,7 +4,7 @@
 //USEUNIT CommonMethod
 
 var DeviceManagementToolbar= CommonMethod.RibbonToolbar
-
+//This method is to Click on Retrieve CConfiguration button
 function ClickonRetrieveConfig()
 {
   if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
@@ -12,7 +12,25 @@ function ClickonRetrieveConfig()
     DeviceManagementToolbar.ClickItem("Device &Management")
     DeviceManagementToolbar.ClickItem("Device &Management|Configuration|&Retrieve Configuration")
     Log.Message("Clicked on Retrieve Configuration Page")
-    CommonMethod.CheckActivityLog("Configuration retrieved  Successfully")    
+    CommonMethod.CheckActivityLog("Configuration retrieved successfully from device")    
+    return true
+  }
+  else
+  {
+    Log.Message("Unable to Click on Device Management Button")
+    return false
+  }
+}
+
+//This method is to Click on Modify CConfiguration button
+function ClickonModifyConfig()
+{
+  if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
+  {
+    DeviceManagementToolbar.ClickItem("Device &Management")
+    DeviceManagementToolbar.ClickItem("Device &Management|Configuration|Modify Co&nfiguration")
+    Log.Message("Clicked on Modify Configuration Page")
+    CommonMethod.CheckActivityLog("Configuration retrieved successfully from device")    
     return true
   }
   else
