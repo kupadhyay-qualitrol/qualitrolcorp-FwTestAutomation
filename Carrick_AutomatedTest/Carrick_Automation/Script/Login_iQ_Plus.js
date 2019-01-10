c8f5ff22dc03e4631fed28f8a84153a7e8ebe10f
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-10 15:18:41 +0530

    AIP-272 Created File for Login Methods

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Login_iQ_Plus.js
@@ -0,0 +1,12 @@
+﻿/*This file contains methods related to Login/Logout iQ+*/
+
+//USEUNIT CommonMethod
+//USEUNIT AssertClass
+//USEUNIT LoginPage
+
+function Login_iQ()
+{
+   var DataSheetName = Project.ConfigPath +"TestData\\SmokeTestData.xlsx"
+   AssertClass.IsTrue(CommonMethod.Launch_iQ_Plus(),"Launch iQ+ application")   
+   LoginPage.Login(CommonMethod.ReadDataFromExcel(DataSheetName,"Username"),CommonMethod.ReadDataFromExcel(DataSheetName,"Password"))  
+}
