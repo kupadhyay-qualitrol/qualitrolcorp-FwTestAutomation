7ce8b4f02bff3012b34a8a0f8f1e12cc84b3b95b
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-10 15:27:05 +0530

    AIP-272 Added Log Message and made correction in name

--- Carrick_AutomatedTest/Carrick_Automation/Script/Download_DFR_Record.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Download_DFR_Record.js
@@ -7,6 +7,7 @@
 //USEUNIT AssertClass
 //USEUNIT ConfigEditorPage
 //USEUNIT ConfigEditor_FaultRecordingPage
+//USEUNIT FavoritesPage
 
 //TC-Test to Download Manual DFR record
 function DownloadManualDFR()
@@ -26,22 +27,24 @@ function DownloadManualDFR()
       if(DataRetrievalPage.GetCOTForLatestDFRRecord()=="MANUAL")
 {     
       REC_DFR=DataRetrievalPage.GetLatestRecordnumber()
-      DeviceManagementPage.ClickOnDownloadDataNow()
+      DataRetrievalPage.ClickOnDownloadDataNow()
       CommonMethod.CheckActivityLog("DFR records saved successfully for device")
       DataRetrievalPage.CloseDFRDirectory() 
       Log.Message("DFR data download")
       //Step3. Click on device Status view option
-       DeviceManagementPage.ClickOnDeviceStatusView()
+       DataRetrievalPage.ClickOnDeviceStatusView()
        Log.Message("Device Status window is open")
        
        //Step4. Get the Current Date time from the device
-       NewDateTime=DeviceManagementPage.GetDeviceCurrentDateTime()
+       NewDateTime=DataRetrievalPage.GetDeviceCurrentDateTime()
        Log.Message("Stores the Device Current date and time")
        
        //Step5. Set Start date time and End date time in IQ+
        TICPage.SetDeviceDateTime(NewDateTime)
        Log.Message("Start Date time and End date time is updated in IQ+")
        
+       //Step5.1 Click on All FR Record Default Favorites
+       AssertClass.IsTrue(FavoritesPage.ClickOnAllFRTriggeredRecord(),"Clicked on All FR Triggered Record")
      
       //Step6. Verify downloaded record on PDP
         REC=PDPPage.VerifyDownloadedRecord()
@@ -51,7 +54,7 @@ function DownloadManualDFR()
         }
         else
         {
-          Log.Message("Fail: DFR latest record not able to downloaded and verified on PDP")
+          Log.Error("Fail: DFR latest record not able to downloaded and verified on PDP")
         }  
 }
       else
@@ -75,6 +78,16 @@ function Validate_RecordTime()
   {
     Log.Message("Start:-Test to Validate Prefault,Post fault time and record length in the DFR record.")
     var DataSheetName = Project.ConfigPath +"TestData\\SmokeTestData.xlsx"
+    //Step0.Check whether device exists or not in the topology.    
+    if(DeviceTopologyPage.ClickonDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"))!=true)
+    {
+      GeneralPage.CreateDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceSerialNo"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceIPAdd"))
+      DeviceTopologyPage.ClickonDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"))      
+    }
+    else
+    {
+      Log.Message("Device exist in the tree topology.")
+    }
     
     //Step1. Retrieve Configuration
     AssertClass.IsTrue(DeviceManagementPage.ClickonRetrieveConfig())
@@ -87,33 +100,35 @@ function Validate_RecordTime()
     AssertClass.IsTrue(ConfigEditor_FaultRecordingPage.SetPrefault(prefault))
     
     //Step4. Set Post-fault time for External Triggers
-    var postfault=CommonMethod.ReadDataFromExcel(DataSheetName,"PostfaultTime")
+    var postfault=CommonMethod.ReadDataFromExcel(DataSheetName,"PostFaultTime")
     AssertClass.IsTrue(ConfigEditor_FaultRecordingPage.SetPostFault(postfault))
     
     //Step5. Send to Device
     AssertClass.IsTrue(ConfigEditorPage.ClickSendToDevice())
     
     //Step6. Download Manual DFR Record
-    Download_DFR_Record()
+    DownloadManualDFR()
     
     //Step7. Check Record Length
     var RecordLength= CommonMethod.ConvertTimeIntoms(PDPPage.GetRecordDuration())
-    AssertClass.CompareDecimalValues(aqConvert.StrToInt64(prefault)+aqConvert.StrToInt64(postfault),RecordLength)
+    AssertClass.CompareDecimalValues(aqConvert.StrToInt64(prefault)+aqConvert.StrToInt64(postfault),aqConvert.StrToInt64(RecordLength),0,"Validating Record Duration.")
     
     //Step8. Check Time Quality Status
     //Step8.1 Click on Device Status View
     AssertClass.IsTrue(DataRetrievalPage.ClickOnDeviceStatusView())
     
     //Step8.2 Check Time Quality Staus Actual
-    AssertClass.CompareString(DataRetrievalPage.TimeQualityStatusFromDeviceStatus(),PDPPage.GetTimeQualityStatus(),"Comparing Time Quality from Device Status and from DFR record in PDP.")
+    AssertClass.CompareString(DataRetrievalPage.TimeQualityStatusFromDeviceStatus(),PDPPage.GetTimeQualityStatus().ToString().OleValue,"Comparing Time Quality from Device Status and from DFR record in PDP.")
     
+    //Step8.3
+    DataRetrievalPage.CloseDeviceStatus.ClickButton()
     //Step9. Check Prefault time
-    var ActualPrefault = (PDPPage.GetRecordTriggerDateTime()+1)-PDPPage.GetRecordStartDateTime()
-    AssertClass.CompareDecimalValues(aqConvert.StrToInt64(prefault),ActualPrefault,"Prefault calculated from PDP is :-"+ActualPrefault)
+    var ActualPrefault = (PDPPage.GetRecordTriggerDateTime())-PDPPage.GetRecordStartDateTime()
+    AssertClass.CompareDecimalValues(aqConvert.StrToInt64(prefault),ActualPrefault,0,"Prefault calculated from PDP is :-"+ActualPrefault)
     
     //Step10. Check Postfault value
-    var ActualPostFault = PDPPage.GetRecordEndDateTime()-(PDPPage.GetRecordTriggerDateTime()+1)
-    AssertClass.CompareDecimalValues(aqConvert.StrToInt64(postfault),ActualPostFault,"Prefault calculated from PDP is :-"+ActualPostFault)
+    var ActualPostFault = PDPPage.GetRecordEndDateTime()-(PDPPage.GetRecordTriggerDateTime())
+    AssertClass.CompareDecimalValues(aqConvert.StrToInt64(postfault),ActualPostFault,0,"Postfault calculated from PDP is :-"+ActualPostFault)
     
     Log.Message("Pass:-Test to Validate Prefault,Post fault time and record length in the DFR record.")
   }
@@ -123,4 +138,3 @@ function Validate_RecordTime()
     Log.Error("Error:-Test to Validate Prefault,Post fault time and record length in the DFR record.")  
   }
 }
-
