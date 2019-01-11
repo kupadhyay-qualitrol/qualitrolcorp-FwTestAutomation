bee968f32ec80a8969bf00d8bd961dbdac11f4a1
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-11 11:57:00 +0530

    AIP-274 Method for Saving CDF File

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/FRViewerPage.js
@@ -0,0 +1,27 @@
+﻿/*This file contains methods and Objects related to FRViewer*/
+
+//USEUNIT AssertClass
+
+//Variables
+var FRViewerFileOption = Aliases.iQ_Plus
+var Edtbx_SaveAs_Name=Aliases.iQ_Plus.dlgSaveAs.DUIViewWndClassName.Explorer_Pane.FloatNotifySink.ComboBox
+var Dialog_SaveAs=Aliases.iQ_Plus.dlgSaveAs
+var Btn_Save =Aliases.iQ_Plus.dlgSaveAs.btnSave
+//
+//This function is used to save the cdf file 
+function SaveToCDFFile(Path)
+{
+  FRViewerFileOption.MainForm.StripMainMenu.Click("File|Save As")
+  if(Dialog_SaveAs.Exists)
+  {
+    Edtbx_SaveAs_Name.SetText(aqString.Trim(Path+"DFRRecord_"+aqString.Replace(aqConvert.TimeIntervalToStr(aqDateTime.Time()),":","_")))
+    Btn_Save.ClickButton()
+    FRViewerFileOption.MainForm.Close()
+    return true
+  }
+  else
+  {
+    Log.Message("Save As dialog is not open.")
+    return false
+  }  
+}
