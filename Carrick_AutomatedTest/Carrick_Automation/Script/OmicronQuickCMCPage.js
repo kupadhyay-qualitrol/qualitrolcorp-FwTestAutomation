326c7ca20d37bbefb30d4ed1d96292e8e72317e7
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-18 14:34:46 +0530

    AIP-68 AIP365 Omicron Quick CMC Page Methods

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/OmicronQuickCMCPage.js
@@ -0,0 +1,22 @@
+﻿/*This file contains methods and objects related to Quick CMC Utility.
+*/
+
+//Variables
+var Btn_Start =Aliases.QuickCMC.wndAfx.xtpBarTop.toolbarTheRibbon.buttonStart
+//
+
+//This function is used to click on Start Button on Quick CMC page
+function ClickStart()
+{
+  if(Btn_Start.Exists)
+  {
+    Btn_Start.ClickButton()
+    Log.Message("Clicked on Start Button on Quick CMC")
+    return true
+  }
+  else
+  {
+    Log.Message("Button Start doesn't exists")
+    return false
+  }
+}
