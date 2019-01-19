e0320579cf0751366486e93eca723b2c8eecc0d4
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-20 01:12:24 +0530

    AIP-305 AIP-348 Test Script for CAM_690

--- Carrick_AutomatedTest/Carrick_Automation/Script/Max_DFR_TestCases.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Max_DFR_TestCases.js
@@ -117,3 +117,65 @@ function CAM_728()
     Log.Error("Fail:-Test to check that DFR record length value get saved to database.")
   }
 }
+
+/*
+CAM-690 Test to check that minimum and maximum limit for DFR record length with non-Transco licenses
+PreCondition- iQ+ is launched already
+*/
+function CAM_690()
+{
+  try
+  {
+    Log.Message("Started TC:- Test to check that minimum and maximum limit for DFR record length with non-Transco licenses ")
+    var DataSheetName = Project.ConfigPath +"TestData\\MaxDFR.xlsx"
+        
+    //Step1.: Check if iQ-Plus is running or not.
+    AssertClass.IsTrue(CommonMethod.IsExist("iQ-Plus"),"Checking if iQ+ is running or not")
+    
+    //Step2.Check whether device exists or not in the topology.    
+    if(DeviceTopologyPage.ClickonDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"))!=true)
+    {
+      GeneralPage.CreateDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceSerialNo"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceIPAdd"))
+      DeviceTopologyPage.ClickonDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"))      
+    }
+    else
+    {
+      Log.Message("Device exist in the tree topology.")
+    }
+    
+    //Step3. Retrieve Configuration
+    AssertClass.IsTrue(DeviceManagementPage.ClickonRetrieveConfig(),"Clicked on Retrieve Config")
+    
+    //Step4. Click on Fault Recording
+    AssertClass.IsTrue(ConfigEditorPage.ClickOnFaultRecording(),"Clicked on Fault Recording")
+    
+    //Step5.0 //Enter PreFault
+    AssertClass.IsTrue(ConfigEditor_FaultRecordingPage.SetPrefault(CommonMethod.ReadDataFromExcel(DataSheetName,"PrefaultTime")),"Setting Prefault Time")
+    
+    //Step5. Enter & Check Max DFR value
+    var MaxDFRLength =CommonMethod.ReadDataFromExcel(DataSheetName,"MaxDFR")
+    AssertClass.IsTrue(ConfigEditor_FaultRecordingPage.SetMaxDFR(MaxDFRLength),"Setting and checking Max DFR")
+    
+    //Step6. Send to Device
+    AssertClass.IsTrue(ConfigEditorPage.ClickSendToDevice(),"Clicked on Send to Device")
+    
+    //Step7. Retrieve Configuration
+    AssertClass.IsTrue(DeviceManagementPage.ClickonRetrieveConfig(),"Clicked on Retrieve Config")
+    
+    //Step8. Click on Fault Recording
+    AssertClass.IsTrue(ConfigEditorPage.ClickOnFaultRecording(),"Clicked on Fault Recording")
+    
+    //Step9. Check for Prefault and Max DFR Value
+    AssertClass.CompareString(ConfigEditor_FaultRecordingPage.GetMaxDFR(), MaxDFRLength,"Checking for Max DFR Value")
+    
+    //Step10. Close Configuration Editor
+    AssertClass.IsTrue(ConfigEditorPage.ClickOnClose(),"Clicked on Close on config Editor")    
+    
+    Log.Message("Pass:- Test to check that minimum and maximum limit for DFR record length with non-Transco licenses")
+  }
+  catch(ex)
+  {
+    Log.Error(ex.stack)
+    Log.Error("Fail:-Test to check that minimum and maximum limit for DFR record length with non-Transco licenses")
+  }
+}
