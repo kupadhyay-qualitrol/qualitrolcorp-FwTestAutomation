9002451cca025a4bda05eb199d5aa0da39f17e20
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-19 15:55:52 +0530

    AIP-303 AIP-340 Added Test Script for CAM-727

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Max_DFR_TestCases.js
@@ -0,0 +1,54 @@
+﻿/*This unit contains test cases related to Max DFR functionality*/
+
+//USEUNIT AssertClass
+//USEUNIT CommonMethod
+//USEUNIT DeviceTopologyPage
+//USEUNIT GeneralPage
+//USEUNIT ConfigEditorPage
+//USEUNIT ConfigEditor_FaultRecordingPage
+
+/*
+CAM-727 Test to check the GUI(Text/Editbox) of iQ+ for Maximum DFR record length
+PreCondition- iQ+ is launched already
+*/
+function CAM_727()
+{
+  try
+  {
+    Log.Message("Started TC:- Test to check the GUI(Text/Editbox) of iQ+ for Maximum DFR record length")
+    var DataSheetName = Project.ConfigPath +"TestData\\MaxDFR.xlsx"
+    
+    //Step1.: Check if iQ-Plus is running or not.
+    AssertClass.IsTrue(CommonMethod.IsExist("iQ-Plus"),"Checking if iQ+ is running or not")
+    
+    //Step2.Check whether device exists or not in the topology.    
+    if(DeviceTopologyPage.ClickonDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"))!=true)
+    {
+      GeneralPage.CreateDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceSerialNo"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceIPAdd"))
+      DeviceTopologyPage.ClickonDevice(CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceType"),CommonMethod.ReadDataFromExcel(DataSheetName,"DeviceName"))      
+    }
+    else
+    {
+      Log.Message("Device exist in the tree topology.")
+    }
+    
+    //Step3. Retrieve Configuration
+    AssertClass.IsTrue(DeviceManagementPage.ClickonRetrieveConfig(),"Clicked on Retrieve Config")
+    
+    //Step4. Click on Fault Recording
+    AssertClass.IsTrue(ConfigEditorPage.ClickOnFaultRecording(),"Clicked on Fault Recording")
+    
+    //Step5.1. Check the Max DFR label.
+    AssertClass.CompareString("Maximum Record Length:", aqString.Trim(ConfigEditor_FaultRecordingPage.GetMaxDFRLabel()),"Checking Max DFR label on UI")
+    
+    //Step5.2. Check the Max DFR Unit.
+    AssertClass.CompareString("ms", aqString.Trim(ConfigEditor_FaultRecordingPage.GetMaxDFRUnit()),"Checking Max DFR unit label on UI")
+    
+    //Step6. Check Max DFR Editbox exist on UI.
+    AssertClass.IsTrue(ConfigEditor_FaultRecordingPage.Edtbx_MaxDFR.Exists,"Checking Editbox exists on UI")
+  }
+  catch(ex)
+  {
+    Log.Error(ex.stack)
+  }
+}
