41dce8534b1110094d1a62433b7c24fac196cef4
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-01-21 14:38:17 +0530

    AIP-303 AIP-340 Added method to close config editor

--- Carrick_AutomatedTest/Carrick_Automation/Script/Max_DFR_TestCases.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Max_DFR_TestCases.js
@@ -47,6 +47,9 @@ function CAM_727()
     //Step6. Check Max DFR Editbox exist on UI.
     AssertClass.IsTrue(ConfigEditor_FaultRecordingPage.Edtbx_MaxDFR.Exists,"Checking Editbox exists on UI")
     Log.Message("Pass:-Test to check the GUI(Text/Editbox) of iQ+ for Maximum DFR record length")
+    
+    //Step7.Close Config Editor
+    AssertClass.IsTrue(ConfigEditorPage.ClickOnClose(),"Clicked on Close in Config Editor")
   }
   catch(ex)
   {
