6814a600d7e45866232b627e8089ab8a287591c5
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-02-01 01:08:33 +0530

    AIP-400 AIP-443 Test Script for Time Zone

--- Carrick_AutomatedTest/Carrick_Automation/Script/TimeZone_TestCases.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/TimeZone_TestCases.js
@@ -5,6 +5,9 @@
 //USEUNIT AssertClass
 //USEUNIT GeneralPage
 //USEUNIT DeviceManagementPage
+//USEUNIT ConfigEditorPage
+//USEUNIT ConfigEditor_TimeManagementPage
+//USEUNIT DataRetrievalPage
 
 
 /*
@@ -15,7 +18,7 @@ function BTC_95_BTC_106()
   try
   {
     Log.Message("Started TC:-Test all Timezone supported in Carrick & Cashel")
-    var DataSheetName = Project.ConfigPath +"TestData\\TimeZone.xlsx"
+    var DataSheetName = Project.ConfigPath +"TestData\\BTC_95_BTC106.xlsx"
     
     //Step1.: Check if iQ-Plus is running or not.
     AssertClass.IsTrue(CommonMethod.IsExist("iQ-Plus"),"Checking if iQ+ is running or not")
@@ -35,7 +38,29 @@ function BTC_95_BTC_106()
     AssertClass.IsTrue(DeviceManagementPage.ClickonRetrieveConfig(),"Clicked on Retrieve Config")
     
     //Step4.Click on Time Management Tab
+    AssertClass.IsTrue(ConfigEditorPage.ClickOnTimeManagement(),"Clicked on Time Management")
     
+    //Step5. Set TimeZone as per Test Data
+    AssertClass.IsTrue(ConfigEditor_TimeManagementPage.SetTimeZone(CommonMethod.ReadDataFromExcel(DataSheetName,"Timezone")),"Sets the Time Zone")
+    
+    //Step6. Click on Send to Device
+    AssertClass.IsTrue(ConfigEditorPage.ClickSendToDevice())
+    
+    //Step7. Retrieve the Device Status
+    AssertClass.IsTrue(DataRetrievalPage.ClickOnDeviceStatusView())
+        
+    //Step8. Set PC Time to Test Data TimeZone
+    CommonMethod.SetPCTimeZone(CommonMethod.ReadDataFromExcel(DataSheetName,"Timezone_path"))
+    
+    //Step9. Get Time
+    var DeviceTime = DataRetrievalPage.GetDeviceActualDateTime()
+    
+    //Step10. Check TimeDifference between iq+ & PC
+    var TimeInterval=aqDateTime.TimeInterval(DeviceTime, aqDateTime.Now())
+    
+    AssertClass.CompareDecimalValues(0,aqDateTime.GetDay(TimeInterval),0,"Checking for Days Difference")
+    AssertClass.CompareDecimalValues(0,aqDateTime.GetHours(TimeInterval),0,"Checking for Hours Difference")
+    AssertClass.CompareDecimalValues(0,aqDateTime.GetMinutes(TimeInterval),10,"Checking for Minutes Difference")
     
     Log.Message("Pass:-Test all Timezone supported in Carrick & Cashel")
   }
