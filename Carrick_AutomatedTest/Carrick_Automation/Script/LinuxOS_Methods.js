898269918bf939f4f9a8d5b075b61152074cda4e
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-02-06 21:40:45 +0700

    AIP-400 AIP-443 Added method to make connection with Linux OS

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/LinuxOS_Methods.js
@@ -0,0 +1,31 @@
+﻿/*This file contains methods to interact with Linux OS for performing device operation*/
+
+//Variables
+var PasswordInfo
+var SSHClient
+//
+
+//This method is used to make SSH connection with device
+function ConnectToDevice(deviceIP,UserName,Password)
+{
+  PasswordInfo = new dotNET.Renci_SshNet.PasswordConnectionInfo.zctor(deviceIP,UserName,Password)                         
+  SSHClient = new dotNET.Renci_SshNet.SshClient.zctor(PasswordInfo)
+  SSHClient.Connect() //method returns void
+  Log.Message("Created SSH Connection with device.")
+  return true
+}
+
+//This method is used to Send Linux command to device and return output
+function SendCommand(Command)
+{
+  var Output = SSHClient.RunCommand(Command)
+  Log.Message("Output from Device is :- "+ Output.Result)
+  return Output.Result
+}
+
+//This method is used to disconnect the SSH connection with Device
+function DisconnectFromDevice()
+{
+  SSHClient.Disconnect()
+  return true
+}
