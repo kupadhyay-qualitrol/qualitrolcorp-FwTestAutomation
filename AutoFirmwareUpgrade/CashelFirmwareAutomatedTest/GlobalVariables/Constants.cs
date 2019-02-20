11f975764e8714fbe0c2d17effddb74f2729eb1e
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-02-20 14:44:19 +0530

    AIP-514 AIP-614 Added new file of constant variables

---
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/GlobalVariables/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashelFirmware.GlobalVariables
+{
+    public static class Constants
+    {
+        public const string glb_DeviceIP_18Channel = "10.75.58.51";
+        public const string glb_DeviceIP_9Channel = "10.75.58.54";
+        public static int glb_deviceType;
+    }
+}
