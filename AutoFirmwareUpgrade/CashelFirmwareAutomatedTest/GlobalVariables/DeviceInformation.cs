e299a1e0b65249d8247a0567bde5b144a2bd37ef
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-02-25 12:45:31 +0530

    AIP-515 AIP-562 Renamed & added new variable for base project path

---
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/GlobalVariables/DeviceInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CashelFirmware.GlobalVariables
+{
+    public static class DeviceInformation
+    {
+        public static readonly string glb_DeviceIP_18Channel = ConfigurationSettings.AppSettings.Get("DeviceIPAddress_18Channel");
+        public static readonly string glb_DeviceIP_9Channel = ConfigurationSettings.AppSettings.Get("DeviceIPAddress_9Channel");
+        public static int glb_deviceType;
+        public static readonly string BaseDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
+    }
+}
