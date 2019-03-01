589bd644c44b50f3b69f8ee48dbf20a8abbcb7cc
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-03-01 16:14:48 +0530

    AIP-631 AIP-632 Added Methods for Selenium Webdriver & Upload Calibration file

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Firmware_Mfgindex_Methods.js
@@ -0,0 +1,26 @@
+﻿/*This file contains methods related to Mfgindex Page of firmware*/
+
+//This method is used to upload the calibration in the device using mfgindex page
+function UploadCalibration(deviceIP,ChromeDriverInstance,CalibrationFilePath)
+{
+ if(deviceIP!=null && ChromeDriverInstance!=null && CalibrationFilePath!=null)
+ {
+  var objTestCode = dotNET.CashelFirmware_NunitTests.FirmwareCablingTest.zctor()
+  var IsCalibrationUploaded = objTestCode.UploadCalbirationFile(deviceIP,ChromeDriverInstance, CalibrationFilePath)
+  if(IsCalibrationUploaded)
+  {
+    Log.Message("Successfull uploaded Calibration file.Result:- "+IsCalibrationUploaded)  
+    return true
+  }
+  else
+  {
+    Log.Message("Failed to load Calibration file.Result:- "+IsCalibrationUploaded)  
+    return false
+  }
+ }
+ else
+ {
+   Log.Message("Either of three are null")
+   return false
+ }
+}
---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/SeleniumWebdriver.js
@@ -0,0 +1,8 @@
+﻿/*This file contains methods and objects related to Firmware Mfgindex page*/
+
+function InitialiseWebdriver(deviceIP)
+{
+  var objBaseTestSuite = dotNET.CashelFirmware_TestSuite.BaseTestSuite.zctor(deviceIP)
+  var ChromeDriverInstance = objBaseTestSuite.TestSetup(Project.ConfigPath+"ThirdParty\\bin\\DLL\\")
+  return ChromeDriverInstance
+}
