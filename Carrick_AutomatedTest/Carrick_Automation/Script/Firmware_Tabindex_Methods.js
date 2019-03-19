c1ae600b798ef174c071c98a87de9cebf2ae4a85
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-03-19 20:22:49 +0530

    AIP-629 AIP-640 Added Method to validate Cabling Parameters

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Firmware_Tabindex_Methods.js
@@ -0,0 +1,25 @@
+﻿/*This file contains methods related to Tabindex Page of firmware*/
+
+function ValidateCabling(ChromeDriverInstance,TestLog,DeviceIP,DataSetFolderPath,Cabling)
+{
+ if(DeviceIP!=null && ChromeDriverInstance!=null && DataSetFolderPath!=null && DataSetFolderPath!=null && Cabling!=null)
+ {
+  var objTestCode = dotNET.CashelFirmware_NunitTests.FirmwareCablingTest.zctor()
+  var Validate = objTestCode.ValidateCabling(ChromeDriverInstance,DeviceIP,TestLog,Cabling,DataSetFolderPath)
+  if(Validate)
+  {
+    Log.Message("Validated Cabling on Firmware.Result:- "+Validate)  
+    return true
+  }
+  else
+  {
+    Log.Message("Failed to Validate Cabling on Firmware.Result:- "+Validate)  
+    return false
+  }
+ }
+ else
+ {
+   Log.Message("Either of input parameter are null")
+   return false
+ }
+}
