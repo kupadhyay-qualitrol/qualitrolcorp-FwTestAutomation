e22b4b4281c60e40e36ad0b5eed80bdaad53e88f
kupadhyay-qualitrol <42198412+kupadhyay-qualitrol@users.noreply.github.com>
2019-05-28 16:43:45 +0530

    Added Import congif as file function and also removed som unwanted codes from Export config function

--- Carrick_AutomatedTest/Carrick_Automation/Script/DeviceManagementPage.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/DeviceManagementPage.js
@@ -18,6 +18,7 @@ var Address_bar = Aliases.iQ_Plus.dlg.WorkerW.ReBarWindow32.AddressBandRoot.prog
 var Confirm_Save_As = Aliases.iQ_Plus.dlgConfirmSaveAs
 var Save_As_Button = Aliases.iQ_Plus.dlgConfirmSaveAs.Confirm_Save_As.CtrlNotifySink.btnYes
 var export_window = Aliases.iQ_Plus.dlg
+var Import_window = Aliases.iQ_Plus.dlgSelectACashelMConfigurationFile
 //
 
 //This method is to Click on Retrieve CConfiguration button
@@ -74,13 +75,10 @@ function ExportConfiguration()
     DeviceManagementToolbar.ClickItem("Device &Management|Configuration|&Retrieve Configuration")
     Log.Message("Clicked on Retrieve Configuration Page")
     CommonMethod.CheckActivityLog("Configuration retrieved successfully from device")
-    aqUtils.Delay(5000)
     Item_ConfigurationEditor.SetFocus()
     Item_ConfigurationEditor.StripMainMenu.Click("Configuration|Export|As a File")
     Log.Message("Export as a file button clicked")
     export_window.SetFocus()
-//    Address_bar.Click()
-//    Address_bar.SetText(aqString.Trim(ConfigurationFilePath))
     ViewFileName.SetText(aqString.Trim("Configuration"))
     dlg.btnSave.Click()
     if(Confirm_Save_As.Exists)
@@ -98,4 +96,28 @@ function ExportConfiguration()
     return false
   }
 }
-
+// This method is to import config from device
+function ImportConfiguration()
+{
+  if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
+  {
+    //Clear Session log  
+    SessionLogPage.ClearLog()
+    DeviceManagementToolbar.ClickItem("Device &Management")
+    DeviceManagementToolbar.ClickItem("Device &Management|Configuration|&Retrieve Configuration")
+    Log.Message("Clicked on Retrieve Configuration Page")
+    CommonMethod.CheckActivityLog("Configuration retrieved successfully from device")
+    Item_ConfigurationEditor.SetFocus()
+    Item_ConfigurationEditor.StripMainMenu.Click("Configuration|Import|From File")
+    Log.Message("Import from file button clicked")
+    Import_window.SetFocus()
+    Import_window.OpenFile("D:\\Belfast\\Configuration\\Configuration.cfg", "CFG files (*.cfg)")
+    ConfigEditorPage.ClickSendToDevice()
+    return true
+  }
+  else
+  {
+    Log.Message("Unable to Click on Device Management Button")
+    return false
+  }
+}
