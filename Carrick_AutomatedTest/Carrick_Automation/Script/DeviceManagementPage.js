821783e38cb2c054ab3afbae33dfea177c2fb875
kupadhyay-qualitrol <42198412+kupadhyay-qualitrol@users.noreply.github.com>
2019-05-29 14:46:34 +0530

    Commited code for Import/Export config as a template

--- Carrick_AutomatedTest/Carrick_Automation/Script/DeviceManagementPage.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/DeviceManagementPage.js
@@ -19,6 +19,11 @@ var Confirm_Save_As = Aliases.iQ_Plus.dlgConfirmSaveAs
 var Save_As_Button = Aliases.iQ_Plus.dlgConfirmSaveAs.Confirm_Save_As.CtrlNotifySink.btnYes
 var export_window = Aliases.iQ_Plus.dlg
 var Import_window = Aliases.iQ_Plus.dlgSelectACashelMConfigurationFile
+var Save_as_template = Aliases.iQ_Plus.Form2
+var ok_button = Aliases.iQ_Plus.Form2.Save_As_Template.SaveAsTemplate.btnOk
+var Template_Name_txt_box = Aliases.iQ_Plus.Form2.Save_As_Template.SaveAsTemplate.txtUsername
+var Template = Aliases.iQ_Plus.Form3.Import_Template.ImportTemplate.gbDefaultConfigurations.lstDefaultTemplates
+var Copy_button = Aliases.iQ_Plus.Form3.Import_Template.ImportTemplate.btnImport
 //
 
 //This method is to Click on Retrieve CConfiguration button
@@ -65,7 +70,7 @@ function ClickonModifyConfig()
   }
 }
 // This method is to export config from device
-function ExportConfiguration()
+function ExportConfigurationAsFile()
 {
   if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
   {
@@ -78,9 +83,9 @@ function ExportConfiguration()
     Item_ConfigurationEditor.SetFocus()
     Item_ConfigurationEditor.StripMainMenu.Click("Configuration|Export|As a File")
     Log.Message("Export as a file button clicked")
-    export_window.SetFocus()
+    Save_as_template.SetFocus()
     ViewFileName.SetText(aqString.Trim("Configuration"))
-    dlg.btnSave.Click()
+    ok_button.Click()
     if(Confirm_Save_As.Exists)
     {
       
@@ -97,7 +102,7 @@ function ExportConfiguration()
   }
 }
 // This method is to import config from device
-function ImportConfiguration()
+function ImportConfigurationAsFile()
 {
   if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
   {
@@ -117,6 +122,57 @@ function ImportConfiguration()
   }
   else
   {
+    Log.Message("Unable to Click on Device Management Button")
+    return false
+  }
+}
+function ExportConfigurationAsTemplate()
+{
+  if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
+  {
+    //Clear Session log  
+    SessionLogPage.ClearLog()
+    DeviceManagementToolbar.ClickItem("Device &Management")
+    DeviceManagementToolbar.ClickItem("Device &Management|Configuration|&Retrieve Configuration")
+    Log.Message("Clicked on Retrieve Configuration Page")
+    CommonMethod.CheckActivityLog("Configuration retrieved successfully from device")
+    Item_ConfigurationEditor.SetFocus()
+    Item_ConfigurationEditor.StripMainMenu.Click("Configuration|Export|As a Template")
+    Log.Message("Export as a template button clicked")
+    Save_as_template.SetFocus()
+    Template_Name_txt_box.SetText(aqString.Trim("Device_51"))
+    //Template_Name_txt_box.wText(aqString.Trim("Device_51"))
+    ok_button.ClickButton()
+    Item_ConfigurationEditor.SetFocus()
+    ConfigEditorPage.ClickOnClose()
+    return true
+  }
+  else
+  {
+    Log.Message("Unable to Click on Device Management Button")
+    return false
+  }
+}
+
+function ImportConfigurationAsTemplate()
+{
+ if(DeviceManagementToolbar.wItems.Item("Device &Management").Text=="Device &Management")
+  {
+    //Clear Session log  
+    SessionLogPage.ClearLog()
+    DeviceManagementToolbar.ClickItem("Device &Management")
+    DeviceManagementToolbar.ClickItem("Device &Management|Configuration|&Retrieve Configuration")
+    Log.Message("Clicked on Retrieve Configuration Page")
+    CommonMethod.CheckActivityLog("Configuration retrieved successfully from device")
+    Item_ConfigurationEditor.SetFocus()
+    Item_ConfigurationEditor.StripMainMenu.Click("Configuration|Import|From Template")
+    Template.ClickItem("Device_51")
+    Copy_button.ClickButton()
+    ConfigEditorPage.ClickSendToDevice()
+    return true
+  }  
+ else
+ {
     Log.Message("Unable to Click on Device Management Button")
     return false
   }
