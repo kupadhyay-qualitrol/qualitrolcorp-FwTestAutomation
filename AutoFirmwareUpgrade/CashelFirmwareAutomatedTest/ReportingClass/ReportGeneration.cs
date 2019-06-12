38184f356ef68cf9a9f62b4eb1e23313b7419c2b
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-06-12 10:25:50 +0530

    AIP-1150 AIP-1153 Kept single common method and corrected path setting

--- AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/ReportingClass/ReportGeneration.cs
+++ AutoFirmwareUpgrade/CashelFirmwareAutomatedTest/ReportingClass/ReportGeneration.cs
@@ -14,22 +14,9 @@ namespace CashelFirmware.Reporting
         public static ExtentReports  extent;
         public static IWebDriver webDriver;
  
-        public static void StartReport_dotNet(string TestSuite)
-        {
-            string projectPath = System.IO.Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
-            string reportPath = projectPath + @"\Reports\"+TestSuite+".html";
-
-            extent = new ExtentReports(reportPath, true);
-            extent
-            .AddSystemInfo("Host Name", "QTRL-FLTHQ72")
-            .AddSystemInfo("Environment", "Windows")
-            .AddSystemInfo("User Name", "Rahuldev Gupta");
-            extent.LoadConfig(projectPath + "extent-config.xml");
-        }
-
         public static void StartReport(string TestSuite)
         {
-            string projectPath = new Uri(Environment.CurrentDirectory).LocalPath;
+            string projectPath = System.IO.Directory.GetParent(CashelFirmware.GlobalVariables.DeviceInformation.BaseDirectoryPath).Parent.Parent.FullName.ToString();
             string reportPath = projectPath + @"\Reports\" + TestSuite + ".html";
 
             extent = new ExtentReports(reportPath, true);
