c50d764b033ac5a7178f449b973acb154a71d5f8
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-06-28 18:16:38 +0530

    AIP-1156 AIP-1150 Increased retry count as it takes time to get device status.

--- Carrick_AutomatedTest/Carrick_Automation/Script/DataRetrievalPage.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/DataRetrievalPage.js
@@ -249,7 +249,7 @@ function ClickOnDeviceStatusView()
    aqObject.CheckProperty(Aliases.iQ_Plus.DropDownForm.PopupMenuControlTrusted, "Enabled", cmpEqual, true)
    CommonMethod.RibbonToolbar.ClickItem("Device &Management|Data Retrieval|Device Diagnostic/&Test|D&evice Status")
    Log.Message("Clicked on Device Status Option")
-   retryCnt =2
+   retryCnt =5
    do
    {
     if(CommonMethod.CheckActivityLog("Device information displayed successfully"))
@@ -257,6 +257,7 @@ function ClickOnDeviceStatusView()
       break
     }
     retryCnt = retryCnt-1
+    Log.Message(retryCnt)
    }
    while (retryCnt>0)
    return true
