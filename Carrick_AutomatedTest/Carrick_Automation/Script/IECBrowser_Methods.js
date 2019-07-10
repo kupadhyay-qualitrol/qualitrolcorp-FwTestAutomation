208fd087359947cc9df55844dd15f86b7184f283
kupadhyay-qualitrol <42198412+kupadhyay-qualitrol@users.noreply.github.com>
2019-07-10 15:52:22 +0530

    AIP-1227 AIP-1371 removed unwanted code

--- Carrick_AutomatedTest/Carrick_Automation/Script/IECBrowser_Methods.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/IECBrowser_Methods.js
@@ -53,40 +53,6 @@ function ConnectDeviceToIECBrowser(deviceIPAdd)
   while(!device_connection && protocol_enable_counter<=10)
 	}
 }
-
-//function ConnectDeviceToIECBrowser(deviceIPAdd)
-//{
-//  //Step1. Click on connect button on tool bar
-//  AssertClass.IsTrue(IECBrowserPage.ConnectDevice(),"Clicked on connect button at toolbar of IECbrowser")
-//  
-//  //Step2. Set IP address in IP address text box
-//  AssertClass.IsTrue(IECBrowserPage.SetIPAddress(deviceIPAdd),"IP address set as iedevice IP")
-//  
-//  //Step3. Click on connect button
-//  AssertClass.IsTrue(IECBrowserPage.ClickOnConnectButton(),"Clicked on connect button")
-//  
-//  //Step4. Test the connection
-//  var device_connection = false
-//  var protocol_enable_counter = 0
-//  aqUtils.Delay(3*Referesh_time)
-//  do
-//  {
-//    protocol_enable_counter++
-//    if(IECBrowserCommonMethod.CheckTraceLog("Done"))
-//    {
-//     Log.Message("Connection successful") 
-//     device_connection = true   
-//     aqUtils.Delay(2*Referesh_time)  //Delay for getting page refereshed
-//    }
-//    else if(IECBrowserCommonMethod.CheckTraceLog("Connect failed!"))
-//    {
-//     IECBrowserPage.CleareTraceLog()
-//     Log.Message("Retry connection")
-//     IECBrowser_Methods.ConnectDeviceToIECBrowser(deviceIPAdd)
-//    }  
-//  }
-//  while(!device_connection && protocol_enable_counter<=10)
-//}
 //This method will add protocol and download the cid files
 function VerifyDeviceConnection()
 {
