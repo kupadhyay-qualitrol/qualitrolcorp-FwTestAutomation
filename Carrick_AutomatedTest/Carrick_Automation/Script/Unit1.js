09237e0ea83bd0ca25c6c29b289fb216f1f59096
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-07-18 18:03:41 +0530

    Temp_5

--- Carrick_AutomatedTest/Carrick_Automation/Script/Unit1.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Unit1.js
@@ -1,7 +1,7 @@
 ﻿function Message()
 {
   Log.Message(BuiltIn.ParamCount())
-  Log.Message(DeviceIP_18Channel)
+  
   for (let i=0;i<=BuiltIn.ParamCount();i++ )
   {
     Log.Message(BuiltIn.ParamStr(i))
