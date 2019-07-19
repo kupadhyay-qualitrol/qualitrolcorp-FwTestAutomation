03849904e99d1493402545a4ae32ee482c1af987
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-07-19 14:50:55 +0530

    AIP-1341 AIP-1500 Added code to check for fw version >=4.16

--- Carrick_AutomatedTest/Carrick_Automation/Script/Firmware_Tabindex_Methods.js
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Firmware_Tabindex_Methods.js
@@ -1,11 +1,11 @@
 ﻿/*This file contains methods related to Tabindex Page of firmware*/
 
-function ValidateCabling(ChromeDriverInstance,TestLog,DeviceIP,DataSetFolderPath,Cabling)
+function ValidateCabling(ChromeDriverInstance,TestLog,DeviceIP,DataSetFolderPath,Cabling,FwVersion)
 {
  if(DeviceIP!=null && ChromeDriverInstance!=null && DataSetFolderPath!=null && DataSetFolderPath!=null && Cabling!=null)
  {
   var objTestCode = dotNET.CashelFirmware_NunitTests.FirmwareCablingTest.zctor()
-  var Validate = objTestCode.ValidateCabling(ChromeDriverInstance,DeviceIP,TestLog,Cabling,DataSetFolderPath)
+  var Validate = objTestCode.ValidateCabling(ChromeDriverInstance,DeviceIP,TestLog,Cabling,DataSetFolderPath,FwVersion)
   if(Validate)
   {
     Log.Message("Validated Cabling on Firmware.Result:- "+Validate)  
