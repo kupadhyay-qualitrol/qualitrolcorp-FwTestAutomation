16b42ecf2dabf60e57095fa3aa341ae84fae083e
rdev-qualitrol <rdev@qualitrolcorp.com>
2019-07-22 14:52:12 +0530

    AIP-1341 AIP-1505 Jenkins job with command line arguments
    Code to fetch device details from Jenkins command line arguments

---
+++ Carrick_AutomatedTest/Carrick_Automation/Script/Parameterised_Build.js
@@ -0,0 +1,14 @@
+﻿function Test()
+{
+  Log.Message(BuiltIn.ParamCount())
+  var temp =aqString.Trim(BuiltIn.ParamStr(11))
+  Log.Message(temp)
+  aqString.ListSeparator=";"
+  Log.Message(aqString.GetListLength(temp))
+  for(i=0;i<aqString.GetListLength(temp);i++)
+  {
+    Log.Message(aqString.GetListItem(temp,i))
+  }
+  
+}
+
